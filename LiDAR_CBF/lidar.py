1cfa24d3a8b50f74e43c418c8b0ca8a7fab34f17
amirsaeid254 <amir.safari10@yahoo.com>
2025-06-23 10:54:04 -0700

    Lidar bug fixed!

--- LiDAR_CBF/lidar.py
+++ LiDAR_CBF/lidar.py
@@ -1,4 +1,5 @@
 import torch
+import matplotlib.pyplot as plt
 
 
 class Lidar:
@@ -19,6 +20,7 @@ class Lidar:
 
         # Transform the mesh from robot to world coordinates
         transformed_vectorized_global_mesh = self._robot_to_world(vectorized_global_mesh, pos, rot)
+
         boundary_points, detected_points = self._boundary_extractor(transformed_vectorized_global_mesh)
 
         if self.lidar_cfg.has_noise:
@@ -64,26 +66,76 @@ class Lidar:
     def _boundary_extractor(self, mesh):
         # Calculate barrier values
         z = self.map.map_barrier.min_barrier(mesh)
+
+        # indices = torch.where(torch.abs(z) < 0.01)[0]
+        # neg_ind = torch.where(z < 0)[0]
+        # pos_ind = torch.where(z >= 0.0)[0]
+        # points_close_to_zero = mesh[indices]
+        # pos_points = mesh[pos_ind]
+        # neg_points = mesh[neg_ind]
+        #
+        # xx = torch.linspace(0 - 0.5, 15 + 0.5, 150)
+        # yy = torch.linspace(0 - 0.5, 15 + 0.5, 150)
+        # X, Y = torch.meshgrid(xx, yy)
+        # points = torch.column_stack((X.flatten(), Y.flatten()))
+        # points = torch.column_stack((points, torch.zeros(points.shape)))
+        # Z = self.map.map_barrier.min_barrier(points).reshape(X.shape)
+        # plt.figure(figsize=(8, 6), dpi=100)
+        # plt.contour(X, Y, Z, levels=[0], colors='k')
+        # # plt.scatter(mesh[:, 0], mesh[:, 1], s=1)
+        # plt.scatter(pos_points[:, 0], pos_points[:, 1], c='blue', s = 1)
+        # plt.scatter(neg_points[:, 0], neg_points[:, 1], c='red', s=1)
+        # # plt.scatter(points_close_to_zero[:, 0], points_close_to_zero[:, 1], s=1, color='red')
+        # plt.axis('equal')
+        # plt.show()
+
         # Reshape z and mesh
         batch_size = mesh.shape[0] // (self.lidar_cfg.ray_num * self.lidar_cfg.ray_sampling_rate)
-        reshaped_z = z.view(batch_size, self.lidar_cfg.ray_num, self.lidar_cfg.ray_sampling_rate, 1)
+        reshaped_z = z.view(batch_size, self.lidar_cfg.ray_num, self.lidar_cfg.ray_sampling_rate)
         reshaped_mesh = mesh.view(batch_size, self.lidar_cfg.ray_num, self.lidar_cfg.ray_sampling_rate,
                                   self.lidar_cfg.space_dimension)
 
         # Find boundary_points points
-        sign_change = (reshaped_z[:, :, :-1, :] >= 0) & (reshaped_z[:, :, 1:, :] < 0)
+        sign_change = (reshaped_z[:, :, :-1] >=0) & (reshaped_z[:, :, 1:] < 0)
         change_indices = torch.nonzero(sign_change, as_tuple=True)
 
         boundary_points = reshaped_mesh[:, :, -1, :]
-        detected_points = torch.full(reshaped_mesh[:, :, -1, :].shape, float('nan'), dtype=torch.float64)
+        detected_points = torch.full(boundary_points.shape, float('nan'), dtype=torch.float64)
 
         if change_indices[0].numel() > 0:
             # Find unique ray indices
-            _, unique_indices = torch.unique(torch.stack(change_indices[:2]), dim=1, return_inverse=True)
+            change_indices = torch.stack(change_indices)
+            # Create a tuple of the first two rows
+            key_rows = tuple(change_indices[:2])
+
+            # Use a dictionary to keep track of first occurrences
+            seen = {}
+            keep_indices = []
+
+            for i in range(change_indices.shape[1]):
+                key = (key_rows[0][i].item(), key_rows[1][i].item())
+                if key not in seen:
+                    seen[key] = True
+                    keep_indices.append(i)
+
+            # Use the indices to select the unique columns
+            filtered = change_indices[:, keep_indices]
 
             # Assign boundary_points points
-            detected_points[change_indices[0], change_indices[1]] = reshaped_mesh[
-                change_indices[0], change_indices[1], change_indices[2]]
+            detected_points[filtered[0], filtered[1]] = reshaped_mesh[
+                filtered[0], filtered[1], filtered[2]]
+
+        # xx = torch.linspace(0 - 0.5, 15 + 0.5, 150)
+        # yy = torch.linspace(0 - 0.5, 15 + 0.5, 150)
+        # X, Y = torch.meshgrid(xx, yy)
+        # points = torch.column_stack((X.flatten(), Y.flatten()))
+        # points = torch.column_stack((points, torch.zeros(points.shape)))
+        # Z = self.map.map_barrier.min_barrier(points).reshape(X.shape)
+        # plt.figure(figsize=(8, 6), dpi=100)
+        # plt.contour(X, Y, Z, levels=[0], colors='k')
+        # plt.scatter(detected_points[0, :, 0], detected_points[0, :, 1], s=1)
+        # plt.axis('equal')
+        # plt.show()
         return boundary_points, detected_points
 
 
